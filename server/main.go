5e24c49c89157ad037c64a28a1e4abfd90e37c06
adrianwg23 <adrianwg23@hotmail.com>
2020-05-09 23:20:34 -0400

    return pod ip to client

--- server/main.go
+++ server/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/adrianwg23/grpc-example/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"os"
 )
 
 type server struct{}
@@ -28,14 +30,29 @@ func main() {
 
 func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
 	log.Println("Request served!")
+	ip := getIP()
 	a, b := request.GetA(), request.GetB()
 	result := a + b
-	return &proto.Response{Result: result}, nil
+	return &proto.Response{Result: result, Ip: ip}, nil
 }
 
 func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
 	log.Println("Request served!")
+	ip := getIP()
 	a, b := request.GetA(), request.GetB()
 	result := a * b
-	return &proto.Response{Result: result}, nil
+	return &proto.Response{Result: result, Ip: ip}, nil
+}
+
+func getIP() []byte {
+	host, _ := os.Hostname()
+	addrs, _ := net.LookupIP(host)
+	for _, addr := range addrs {
+		if ipv4 := addr.To4(); ipv4 != nil {
+			fmt.Println("IPv4: ", ipv4)
+			return ipv4
+		}
+	}
+
+	return []byte("no ip")
 }
